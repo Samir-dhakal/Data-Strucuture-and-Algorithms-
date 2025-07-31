62b2937cfca395e87a982df3660c5ddbbf25627c
samir_dhakal <samirdhakal.9279@gamil.com>
2025-07-31 21:41:20 +0530

    minimized error in previous code

--- linked_list/LinkedList1.java
+++ linked_list/LinkedList1.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 // import java.util.Scanner;
 
-public class LinkedList1 extends LinkedList<String> {
+public class LinkedList1 {
     Node head;
 
     class Node {
@@ -16,11 +16,13 @@ public class LinkedList1 extends LinkedList<String> {
             this.data = data;
             this.next = null;
         }
-
     }
 
     // to find out weather the linked list is looped or not
     public void IsLooped(Node head) {
+        if (head == null) {
+            return;
+        }
         Node fast = head;
         Node slow = head;
         while (fast != null && slow != null) {
@@ -36,13 +38,58 @@ public class LinkedList1 extends LinkedList<String> {
 
     }
 
+    // add - first , last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+
+        }
+        currNode.next = newNode;
+
+    }
+
+    // to print the list
+    public void printList() {
+        if (head == null) {
+            System.out.println(" the list is empty ");
+            return;
+
+        }
+        Node currNode;
+        currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+
+        }
+        System.out.println("NULL");
+    }
+
     public static void main(String args[]) {
         LinkedList1 list = new LinkedList1();
         list.addFirst("is ");
         list.addFirst("this ");
         list.addFirst("looped ");
         list.addFirst("LinkedLIst ");
-        System.out.println(list);
+        list.printList();
         list.IsLooped(list.head);
     }
 }
