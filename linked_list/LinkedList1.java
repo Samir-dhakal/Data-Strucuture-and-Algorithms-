bd0cc45e6cb5baa3143262b1cb8d005bfc8c3712
samir_dhakal <samirdhakal.9279@gamil.com>
2025-07-31 18:45:52 +0530

    to check weather the list is looped or not

---
+++ linked_list/LinkedList1.java
@@ -0,0 +1,47 @@
+// package linked_list;
+
+import java.util.LinkedList;
+
+// import java.util.Scanner;
+
+public class LinkedList1 extends LinkedList<String> {
+    Node head;
+
+    class Node {
+
+        Node next;
+        String data;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public void LoopDetector(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while (fast != null && slow != null) {
+            fast = (fast.next).next;
+            slow = slow.next;
+            if (fast == slow) {
+                System.out.println(" list is looped ");
+                return;
+            }
+
+        }
+        System.out.println(" your linked list is not looped ");
+
+    }
+
+    public static void main(String args[]) {
+        LinkedList1 list = new LinkedList1();
+        list.addFirst("is ");
+        list.addFirst("this  ");
+        list.addFirst("looped ");
+        list.addFirst("LinkedLIst");
+        System.out.println(list);
+        list.LoopDetector(list.head);
+    }
+}
